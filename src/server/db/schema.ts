6f4b59084ef224f9085a0af20c3b9ec5323fc253
Trey Granderson <treygranderson@gmail.com>
2024-02-25 22:15:30 -0500

    db: configure relations

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { sql, relations } from "drizzle-orm";
 import {
   boolean,
   bigint,
@@ -22,6 +22,9 @@ export const createTable = mysqlTableCreator(
   (name) => `t3-stack-app-router_${name}`,
 );
 
+/************************************************************
+ * MESSAGES
+ ************************************************************/
 export const messages = createTable(
   "message",
   {
@@ -42,11 +45,31 @@ export const messages = createTable(
   }),
 );
 
+// Relations
+export const messagesRelations = relations(messages, ({ one }) => ({
+  user: one(users, {
+    fields: [messages.userId],
+    references: [users.id],
+  }),
+  chat: one(chats, {
+    fields: [messages.chatId],
+    references: [chats.id],
+  }),
+  aiCharacter: one(aiCharacters, {
+    fields: [messages.aiCharacterId],
+    references: [aiCharacters.id],
+  }),
+}));
+
+/************************************************************
+ * CHATS
+ ************************************************************/
 export const chats = createTable(
   "chat",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
     name: varchar("name", { length: 256 }),
+    userId: bigint("id", { mode: "number" }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
@@ -57,12 +80,17 @@ export const chats = createTable(
   }),
 );
 
-export const chatAiParticipants = createTable("chat_ai_participant", {
-  id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-  chatId: bigint("chat_id", { mode: "number" }).notNull(),
-  aiCharacterId: bigint("ai_character_id", { mode: "number" }).notNull(),
-});
+export const chatsRelations = relations(chats, ({ many, one }) => ({
+  messages: many(messages),
+  users: one(users, {
+    fields: [chats.userId],
+    references: [users.id],
+  }),
+}));
 
+/************************************************************
+ * USERS
+ ************************************************************/
 export const users = createTable(
   "user",
   {
@@ -79,11 +107,21 @@ export const users = createTable(
   }),
 );
 
+export const usersRelations = relations(users, ({ many }) => ({
+  messages: many(messages),
+  chats: many(chats),
+  aiCharacters: many(aiCharacters),
+}));
+
+/************************************************************
+ * AI CHARACTERS
+ ************************************************************/
 export const aiCharacters = createTable(
   "ai_character",
   {
     id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
     name: varchar("name", { length: 100 }).notNull(),
+    createdById: bigint("id", { mode: "number" }),
     personalityType: varchar("personality_type", { length: 50 }), // Example: "Humorous", "Philosophical"
     description: varchar("description", { length: 500 }),
     avatarImage: varchar("avatar_image", { length: 255 }),
@@ -98,7 +136,22 @@ export const aiCharacters = createTable(
   }),
 );
 
-// DELETE
+// RELATIONS
+export const aiCharactersRelations = relations(
+  aiCharacters,
+  ({ many, one }) => ({
+    messages: many(messages),
+    chats: many(chats),
+    users: one(users, {
+      fields: [aiCharacters.createdById],
+      references: [users.id],
+    }),
+  }),
+);
+
+/************************************************************
+ * DELETE ME
+ ************************************************************/
 export const posts = createTable(
   "post",
   {
