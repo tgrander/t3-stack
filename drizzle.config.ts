5327453bfa2c3d3e03e13c7d59f1fcd32999815a
Trey Granderson <treygranderson@gmail.com>
2024-02-28 01:21:42 -0500

    delete comments

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,3 @@
-// import { type Config } from "drizzle-kit";
-
-// import { env } from "~/env";
-
-// export default {
-//   schema: "./src/server/db/schema.ts",
-//   driver: "mysql2",
-//   dbCredentials: {
-//     uri: env.DATABASE_URL,
-//   },
-//   tablesFilter: ["t3-stack-app-router_*"],
-// } satisfies Config;
-
 import { type Config } from "drizzle-kit";
 
 import { env } from "~/env";
