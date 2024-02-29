f81c02c3ecfae28094147a14e84a08374b3e074b
Trey Granderson <treygranderson@gmail.com>
2024-02-29 04:20:13 -0500

    useChatPageParams: use reused zod schemas

--- src/hooks/chat/useChatPageParams.ts
+++ src/hooks/chat/useChatPageParams.ts
@@ -1,21 +1,10 @@
 import { useParams } from "next/navigation";
 import { z } from "zod";
+import { NumberUrlParamSchema } from "~/types";
 
 const ChatPageParamsSchema = z.object({
-  chatId: z
-    .string()
-    .optional()
-    .refine((value) => !isNaN(Number(value)), {
-      message: "Must be a convertible number",
-    })
-    .transform((value) => (value !== undefined ? Number(value) : undefined)),
-  personaId: z
-    .string()
-    .optional()
-    .refine((value) => !isNaN(Number(value)), {
-      message: "Must be a convertible number",
-    })
-    .transform((value) => (value !== undefined ? Number(value) : undefined)),
+  chatId: NumberUrlParamSchema,
+  personaId: NumberUrlParamSchema,
 });
 
 type ChatPageParams = z.infer<typeof ChatPageParamsSchema>;
