025aee6baa2fa6afb8ae387a85c1f6020d92d2c8
Trey Granderson <treygranderson@gmail.com>
2024-02-29 01:58:26 -0500

    useChatPageParams transforms string params into numbers

--- src/hooks/chat/useChatPageParams.ts
+++ src/hooks/chat/useChatPageParams.ts
@@ -2,18 +2,26 @@ import { useParams } from "next/navigation";
 import { z } from "zod";
 
 const ChatPageParamsSchema = z.object({
-  chatId: z.string().nullable(),
-  personaId: z.string().nullable(),
+  chatId: z
+    .string()
+    .optional()
+    .refine((value) => !isNaN(Number(value)), {
+      message: "Must be a convertible number",
+    })
+    .transform((value) => (value !== undefined ? Number(value) : undefined)),
+  personaId: z
+    .string()
+    .optional()
+    .refine((value) => !isNaN(Number(value)), {
+      message: "Must be a convertible number",
+    })
+    .transform((value) => (value !== undefined ? Number(value) : undefined)),
 });
 
 type ChatPageParams = z.infer<typeof ChatPageParamsSchema>;
 
 export const useChatPageParams = (): ChatPageParams => {
   const params = useParams();
-
-  const parseResult = ChatPageParamsSchema.safeParse(params);
-
-  return parseResult.success
-    ? parseResult.data
-    : { chatId: null, personaId: null };
+  const parse = ChatPageParamsSchema.parse(params);
+  return parse;
 };
