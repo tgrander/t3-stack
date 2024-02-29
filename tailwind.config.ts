0404090666bb4068005eda6a306c8716e537b663
Trey Granderson <treygranderson@gmail.com>
2024-02-29 13:31:48 -0500

    add brand, main and base palette colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,43 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        // Brand Palette
+        brand: {
+          1: "rgb(231, 231, 233)",
+          2: "rgb(197, 198, 201)",
+          3: "rgb(151, 153, 158)",
+          4: "rgb(102, 105, 113)",
+          5: "rgb(56, 60, 71)",
+          6: "rgb(12, 17, 30)",
+          7: "rgb(10, 14, 26)",
+          8: "rgb(9, 12, 21)",
+          9: "rgb(7, 10, 17)",
+          10: "rgb(5, 8, 13)",
+        },
+        main: {
+          1: "rgb(233, 240, 255)",
+          2: "rgb(201, 219, 255)",
+          3: "rgb(158, 191, 255)",
+          4: "rgb(113, 161, 255)",
+          5: "rgb(71, 132, 255)",
+          6: "rgb(30, 105, 255)",
+          7: "rgb(26, 89, 217)",
+          8: "rgb(21, 75, 181)",
+          9: "rgb(17, 60, 145)",
+          10: "rgb(13, 47, 115)",
+        },
+        base: {
+          1: "rgb(247, 247, 248)",
+          2: "rgb(236, 236, 238)",
+          3: "rgb(221, 221, 224)",
+          4: "rgb(205, 205, 210)",
+          5: "rgb(190, 190, 197)",
+          6: "rgb(176, 176, 184)",
+          7: "rgb(150, 150, 156)",
+          8: "rgb(125, 125, 131)",
+          9: "rgb(100, 100, 105)",
+          10: "rgb(79, 79, 83)",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
@@ -135,53 +172,53 @@ const blue = [
 ];
 const black = [
   {
-    hex: "#e9e9ea",
-    rgb: "rgb(233, 233, 234)",
+    hex: "#e7e7e9",
+    rgb: "rgb(231, 231, 233)",
     token: "black-1",
   },
   {
-    hex: "#cacbcc",
-    rgb: "rgb(202, 203, 204)",
+    hex: "#c5c6c9",
+    rgb: "rgb(197, 198, 201)",
     token: "black-2",
   },
   {
-    hex: "#a1a1a3",
-    rgb: "rgb(161, 161, 163)",
+    hex: "#97999e",
+    rgb: "rgb(151, 153, 158)",
     token: "black-3",
   },
   {
-    hex: "#757678",
-    rgb: "rgb(117, 118, 120)",
+    hex: "#666971",
+    rgb: "rgb(102, 105, 113)",
     token: "black-4",
   },
   {
-    hex: "#4b4c50",
-    rgb: "rgb(75, 76, 80)",
+    hex: "#383c47",
+    rgb: "rgb(56, 60, 71)",
     token: "black-5",
   },
   {
-    hex: "#242529",
-    rgb: "rgb(36, 37, 41)",
+    hex: "#0c111e",
+    rgb: "rgb(12, 17, 30)",
     token: "black-6",
   },
   {
-    hex: "#1f1f23",
-    rgb: "rgb(31, 31, 35)",
+    hex: "#0a0e1a",
+    rgb: "rgb(10, 14, 26)",
     token: "black-7",
   },
   {
-    hex: "#1a1a1d",
-    rgb: "rgb(26, 26, 29)",
+    hex: "#090c15",
+    rgb: "rgb(9, 12, 21)",
     token: "black-8",
   },
   {
-    hex: "#151517",
-    rgb: "rgb(21, 21, 23)",
+    hex: "#070a11",
+    rgb: "rgb(7, 10, 17)",
     token: "black-9",
   },
   {
-    hex: "#101112",
-    rgb: "rgb(16, 17, 18)",
+    hex: "#05080d",
+    rgb: "rgb(5, 8, 13)",
     token: "black-10",
   },
 ];
@@ -235,6 +272,58 @@ const grey = [
     hex: "#4f4f53",
     rgb: "rgb(79, 79, 83)",
     token: "grey-10",
+  },
+];
+const electricBlue = [
+  {
+    hex: "#e8f0fc",
+    rgb: "rgb(232, 240, 252)",
+    token: "blue-1",
+  },
+  {
+    hex: "#c9daf8",
+    rgb: "rgb(201, 218, 248)",
+    token: "blue-2",
+  },
+  {
+    hex: "#9dbdf2",
+    rgb: "rgb(157, 189, 242)",
+    token: "blue-3",
+  },
+  {
+    hex: "#709fec",
+    rgb: "rgb(112, 159, 236)",
+    token: "blue-4",
+  },
+  {
+    hex: "#4582e6",
+    rgb: "rgb(69, 130, 230)",
+    token: "blue-5",
+  },
+  {
+    hex: "#1c66e1",
+    rgb: "rgb(28, 102, 225)",
+    token: "blue-6",
+  },
+  {
+    hex: "#1857bf",
+    rgb: "rgb(24, 87, 191)",
+    token: "blue-7",
+  },
+  {
+    hex: "#1448a0",
+    rgb: "rgb(20, 72, 160)",
+    token: "blue-8",
+  },
+  {
+    hex: "#103a80",
+    rgb: "rgb(16, 58, 128)",
+    token: "blue-9",
+  },
+  {
+    hex: "#0d2e65",
+    rgb: "rgb(13, 46, 101)",
+    token: "blue-10",
   },
 ];
 
