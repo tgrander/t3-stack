bebb0aa6d5ab89cfa955707ba65866c6ddd29a5e
Trey Granderson <treygranderson@gmail.com>
2024-03-05 06:11:57 -0500

    fix: tagsOnAiChars table .reference()

--- src/server/db/tables/aiCharsOnChatsManyToMany.ts
+++ src/server/db/tables/aiCharsOnChatsManyToMany.ts
@@ -1,5 +1,14 @@
-import { relations } from "drizzle-orm";
-import { varchar, primaryKey } from "drizzle-orm/pg-core";
+import { sql, relations } from "drizzle-orm";
+import {
+  index,
+  pgTableCreator,
+  varchar,
+  timestamp,
+  boolean,
+  json,
+  pgEnum,
+  primaryKey,
+} from "drizzle-orm/pg-core";
 
 import { createTable } from "../createTable";
 import { chats, aiCharacters } from "./index";
@@ -9,10 +18,10 @@ export const aiCharsOnChats = createTable(
   {
     aiCharacterId: varchar("ai_character_id", { length: 36 })
       .notNull()
-      .references(() => chats.id),
+      .references(() => aiCharacters.id),
     chatId: varchar("chat_id", { length: 36 })
       .notNull()
-      .references(() => aiCharacters.id),
+      .references(() => chats.id),
   },
   (table) => ({
     pk: primaryKey({ columns: [table.aiCharacterId, table.chatId] }),
