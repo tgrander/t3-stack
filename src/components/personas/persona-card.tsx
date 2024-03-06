4e8d2b2436a76b197dcfef12a69475732f2e31a3
Trey Granderson <treygranderson@gmail.com>
2024-03-06 12:11:53 -0500

    persona-card: styled blurred glassomorphic footer

--- src/components/personas/persona-card.tsx
+++ src/components/personas/persona-card.tsx
@@ -18,21 +18,26 @@ export function PersonaCard({ persona: p }: Props) {
   return (
     <Card
       key={p.id}
-      className="relative mx-2 h-72 w-72 overflow-hidden rounded-2xl"
+      className="relative mx-2 h-72 w-72 cursor-pointer overflow-hidden rounded-2xl"
     >
-      <div className="absolute left-0 top-0 h-full w-full">
-        <CldImage
-          alt="image"
-          className={cn("m-0 h-full w-full object-cover p-0")}
-          width="300"
-          height="300"
-          src={p.cloudinaryPublicId ?? ""}
-          style={{ objectFit: "cover" }}
-        />
+      <div className="flex h-full flex-col justify-end">
+        <div className="absolute left-0 top-0 h-full w-full">
+          <CldImage
+            alt="image"
+            className={cn("m-0 h-full w-full object-cover p-0")}
+            width="300"
+            height="300"
+            src={p.cloudinaryPublicId ?? ""}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <CardFooter className="border-1 mx-2 mb-2 flex h-auto items-center justify-start overflow-hidden rounded-xl border-white/20 bg-background/10 px-4 py-3 subpixel-antialiased backdrop-blur-sm backdrop-saturate-150 before:rounded-xl before:bg-white/10">
+          <p className="text-tiny text-white/80">{p.name}</p>
+        </CardFooter>
       </div>
-      <CardFooter className="color-inherit border-1 rounded-large shadow-small absolute bottom-1 z-10 ml-1 flex h-auto w-[calc(100%_-_8px)] items-center justify-start overflow-hidden border-white/20 bg-background/10 p-3 py-1 subpixel-antialiased backdrop-blur backdrop-saturate-150 before:rounded-xl before:bg-white/10">
-        <p className="text-tiny text-white/80">Available soon.</p>
-      </CardFooter>
     </Card>
   );
 }
+
+// absolute bottom-1 z-10
+// w-[calc(100%_-_8px)]
