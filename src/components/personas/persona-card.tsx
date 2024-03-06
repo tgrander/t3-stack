3a3e49a677b2eb6320ed3189cb2d9d4acfcbeedd
Trey Granderson <treygranderson@gmail.com>
2024-03-06 10:42:26 -0500

    persona-card: attempt at glassmorphism

--- src/components/personas/persona-card.tsx
+++ src/components/personas/persona-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CldImage } from "next-cloudinary";
-import { Card } from "~/ui/card";
+import { Card, CardFooter } from "~/ui/card";
 import { cn } from "~/utils";
 import { api } from "~/trpc/server";
 
@@ -30,6 +30,9 @@ export function PersonaCard({ persona: p }: Props) {
           style={{ objectFit: "cover" }}
         />
       </div>
+      <CardFooter className="color-inherit border-1 rounded-large shadow-small absolute bottom-1 z-10 ml-1 flex h-auto w-[calc(100%_-_8px)] items-center justify-start overflow-hidden border-white/20 bg-background/10 p-3 py-1 subpixel-antialiased backdrop-blur backdrop-saturate-150 before:rounded-xl before:bg-white/10">
+        <p className="text-tiny text-white/80">Available soon.</p>
+      </CardFooter>
     </Card>
   );
 }
